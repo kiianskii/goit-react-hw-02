d1f40d218cc68dc838c38a349aa53bd6f7cbdf6b
kiianskii <ankiyanskiy@gmail.com>
2024-03-28 13:45:41 +0200

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
-import Description from './Description/Description'
-import Feedback from './Feedback/Feedback'
-import Options from './Options/Options'
+import Description from './components/Description/Description'
+import Feedback from './components/Feedback/Feedback'
+import Options from './components/Options/Options'
 import { useState, useEffect } from "react";
 
 const test = { good: 0, neutral: 0, bad: 0 }
